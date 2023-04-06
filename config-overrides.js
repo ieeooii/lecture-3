e4fd77ddf81d0dd6f6b20d960b8d14093b4689eb
CLOwinnie <winnie@clo3d.com>
2023-04-06 23:43:53 +0900

    feat: font optimization - preload

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
+
+module.exports = function override(config, env) {
+    config.plugins.push(new PreloadWebpackPlugin({
+        rel: 'preload',
+        as: 'font',
+        include: 'allAssets',
+        fileWhiteList: [/(.woff2?)/i]
+    }))
+    return config;
+}
